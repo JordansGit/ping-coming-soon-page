065c447b669cd09c511a9a919b46cf91068d8fa8
Jordan Patel <21124677+JordansGit@users.noreply.github.com>
2022-05-20 15:55:55 +0100

    refactored some js code so I understand it better

--- js/script.js
+++ js/script.js
@@ -1,35 +1,12 @@
 const btn = document.querySelector('button');
-
-
-/*
-btn.addEventListener('submit', function(input) {
-  const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  console.log(input);*//*
-  if (input.value.match(emailFormat)) {
-    return true;
-  } else {
-    errorMsg.textContent = "Please provide a valid email address";
-    return false;
-  }*//*
-})*/
-/* need click event for the submit button. to run this validate email function. */ 
-
-/*
-email.addEventListener('input', function(e) {
-  console.log(e.target.value);
-  console.log(e.target)
-  console.log(e);
-  const input = e.target.value;
-  e.preventDefault();
-})*/
-
-const emailEl = document.getElementById('email');
+const email = document.getElementById('email');
 const form = document.querySelector('form'); 
+const errorMsg = document.getElementById('error-msg');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  let isEmailValid = checkEmail(); 
+  checkEmail(); 
 });
 
 // function to check if email input is empty. 
@@ -41,30 +18,21 @@ const isRequired = function(value) {
   }  
   return value;  
 }; 
-/* 
-the shorthand es6 version of the above code is this: 
-  const isRequired = value => value === '' ? false : true;
-i've broken it down to original js code for learning purposes
-*/ 
-
 
 // function to check if email has a valid input. i.e. xyz@domain.net. 
 const isEmailValid = (x) => {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(x);
+  const emailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailFormat.test(x);
 };
-
-const errorMsg = document.getElementById('error-msg');
 
 // return true if email is provided and it's valid. + show error msg if it returns false. 
 const checkEmail = () => {
   let valid = false;
-  const x = emailEl.value.trim();
+  const x = email.value.trim();
   if (!isRequired(x) || !isEmailValid(x)) {
     errorMsg.textContent = 'Please provide a valid email address'; 
   } else {
     errorMsg.textContent = ''; 
-    alert('valid email');
     valid = true;
   }
   return valid;
@@ -72,11 +40,20 @@ const checkEmail = () => {
 
 
 
-/* example of valid email: java@gmail.com */
-
 /* Notes: 
 
 not sure why you can use both const isEmailValid and let isEmailValid since they are the same???? are they??? 
 
-
 */
+
+/*
+Learning Notes: 
+
+The event listener is broken up into 3 seperate functions. It's not required but it's better practice for bigger projects. 
+
+const isRequired function: 
+  the shorthand es6 version of the above code is this: 
+  const isRequired = value => value === '' ? false : true;
+  i've broken it down to original js code for learning purposes
+*/
+
